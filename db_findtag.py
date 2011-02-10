faa878e31e8cde709c348ecddb6e1607d72dd17e
Carl Drougge <bearded@longhaired.org>
2011-02-10 14:52:23 +0100

    Add db_findtag.py.

---
+++ db_findtag.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+
+from sys import argv, exit
+from dbclient import dbclient
+
+def usage():
+	print "Usage:", argv[0], " [-opts] partial-tag-name"
+	print "  -f: Fuzzy matching"
+	print "  -a: Match anywhere"
+	print "  -s: Short listing (just tagnames)"
+	exit(1)
+
+def short(tag):
+	print tag["name"]
+
+def long(tag):
+	print tag["name"], tag["type"], tag["posts"], tag["weak_posts"]
+
+opts = ""
+known_opts = "fas"
+part = ""
+for a in argv[1:]:
+	if a[0] == "-":
+		for c in a[1:]:
+			if c not in known_opts: usage()
+			opts += c
+	else:
+		if part: usage()
+		part = a
+if not part: usage()
+client = dbclient()
+match = "F" if "f" in opts else "E"
+where = "P" if "a" in opts else "I"
+cmd = match + "A" + where
+printer = short if "s" in opts else long
+map(printer, client.find_tags(cmd, part).itervalues())
