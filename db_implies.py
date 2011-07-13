1e370e4bf320db0adc17e171707329b8840bce94
Carl Drougge <bearded@longhaired.org>
2011-07-13 05:07:55 +0200

    [db_implies] Make it possible to remove an implication too.

--- db_implies.py
+++ db_implies.py
@@ -6,14 +6,18 @@ from dbclient import dbclient
 
 if len(argv) not in (3, 4):
 	print "Usage:", argv[0], "set_tag implied_tag [priority]"
+	print "use -set_tag to remove an implication"
 	exit(1)
 
 client = dbclient()
-set_tag = client.find_tag(argv[1])
+set_tag = client.find_tag(argv[1], with_prefix=True)
 implied_tag = client.find_tag(argv[2], with_prefix=True)
 priority = 0
 if len(argv) == 4: priority = int(argv[3])
 if set_tag and implied_tag:
-	client.add_implies(set_tag, implied_tag, priority)
+	if set_tag[0] == "-":
+		client.remove_implies(set_tag[1:], implied_tag)
+	else:
+		client.add_implies(set_tag, implied_tag, priority)
 else:
 	print "Not found"
