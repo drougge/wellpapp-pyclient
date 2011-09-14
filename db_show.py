e6f880dd3e63c19c0b36723a8386600666c919b3
Carl Drougge <bearded@longhaired.org>
2011-09-14 10:08:22 +0200

    [db_show] Add -q switch, for less verbose output.

--- db_show.py
+++ db_show.py
@@ -21,17 +21,18 @@ def show_implies(guid, heading, reverse):
 	impl = client.tag_implies(guid, reverse)
 	if impl: print heading + "".join(map(implfmt, impl))
 
-def show_post(m):
+def show_post(m, short=False):
 	post = client.get_post(m, True, ["tagname", "ext", "created", "width", "height", "source", "title"])
 	if not post:
 		print "Post not found"
 		return 1
 	t = localtime(post["created"])
 	print m + " created " + strftime("%F %T", t)
-	print post["width"], "x", post["height"], post["ext"]
-	for field in ("title", "source"):
-		if field in post:
-			print field.title() + ": " + _tagenc(post[field])
+	if not short:
+		print post["width"], "x", post["height"], post["ext"]
+		for field in ("title", "source"):
+			if field in post:
+				print field.title() + ": " + _tagenc(post[field])
 	try:
 		path = readlink(client.image_path(m))
 		if not exists(path):
@@ -39,6 +40,7 @@ def show_post(m):
 	except Exception:
 		path = "MISSING"
 	print "Original file: " + path
+	if short: return 0
 	print "Tags:\n\t",
 	print "\n\t".join(map(_tagenc, sorted(post["tagname"])))
 	if post["impltagname"]:
@@ -49,7 +51,7 @@ def show_post(m):
 		print "Related posts:\n\t" + "\n\t".join(rels)
 	return 0
 
-def show_tag(name):
+def show_tag(name, short=False):
 	guid = client.find_tag(name)
 	if not guid and match(r"(?:\w{6}-){3}\w{6}", name):
 		guid = name
@@ -64,6 +66,7 @@ def show_tag(name):
 	if "alias" in data: print "Aliases:", " ".join(map(_tagenc, data["alias"]))
 	print "GUID:", guid
 	print "Type:", _tagenc(data["type"])
+	if short: return 0
 	print data["posts"], "posts"
 	print data["weak_posts"], "weak posts"
 	show_implies(guid, "Implies:", False)
@@ -74,17 +77,23 @@ def show_tag(name):
 	return 0
 
 if __name__ == "__main__":
-	if len(argv) < 2:
+	new_argv = []
+	short = False
+	for a in argv[1:]:
+		if a == "-q":
+			short = True
+		else:
+			new_argv.append(a)
+	if len(new_argv) < 1:
 		print "Usage:", argv[0], "post-spec or tagname [...]"
 		exit(1)
 	client = dbclient()
-	object = client.postspec2md5(argv[1], argv[1])
 	ret = 0
-	for object in argv[1:]:
+	for object in new_argv:
 		object = client.postspec2md5(object, object)
 		if match(r"^[0-9a-f]{32}$", object):
-			ret |= show_post(object)
+			ret |= show_post(object, short)
 		else:
-			ret |= show_tag(object)
-		if len(argv) > 2: print
+			ret |= show_tag(object, short)
+		if len(new_argv) > 1: print
 	exit(ret)
